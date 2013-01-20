01da6a42b6abe779e68a9dbc461b7304fe130621
William Kunkel <kunkel.w.e@gmail.com>
2013-01-20 17:13:27 -0500

    Split CheesyDrive from Drive into its own file

---
+++ Commands/CheesyDrive.h
@@ -0,0 +1,41 @@
+/**
+ * @file Drive.h
+ * @brief Declaration of the CheesyDrive command
+ * @author William Kunkel
+ */
+
+#ifndef CHEESY_DRIVE_H
+#define CHEESY_DRIVE_H
+
+#include "../CommandBase.h"
+
+/**
+ * @brief The CheeseyDrive command
+ *
+ * Drive the robot using "Cheesy Drive", where one of the joysticks controls
+ * forward-and-back motion and the other controls turning.
+ *
+ * @author William Kunkel
+ */
+class CheesyDrive: public CommandBase {
+public:
+	/// Constructor for the CheesyDrive constructor
+	CheesyDrive();
+
+	/// Implement virtual method and do nothing
+	void Initialize() {};
+
+	/// Drive from joysticks
+	void Execute();
+
+	/// Return false
+	bool IsFinished();
+
+	/// Implement virtual method and do nothing
+	void End() {};
+
+	/// Implement virtual method and do nothing
+	void Interrupted() {};
+};
+
+#endif // CHEESY_DRIVE_H
