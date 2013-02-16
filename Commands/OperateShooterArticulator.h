67a6ff60b2a38dc95122ef4b5bf0d68290cdbe93
Nyle <nyle.rodgers@gmail.com>
2013-02-16 16:45:08 -0500

    added operate shooter articulator command

---
+++ Commands/OperateShooterArticulator.h
@@ -0,0 +1,21 @@
+#ifndef OPERATESHOOTERARTICULATOR_H
+#define OPERATESHOOTERARTICULATOR_H
+
+#include "../CommandBase.h"
+
+/**
+ *
+ *
+ * @author Nyle
+ */
+class OperateShooterArticulator: public CommandBase {
+public:
+	OperateShooterArticulator();
+	virtual void Initialize() {};
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End() {};
+	virtual void Interrupted() {};
+};
+
+#endif
