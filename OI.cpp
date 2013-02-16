64fdac43fd96d1f02c28eadb0df18e62f0642f6e
Nyle <nyle.rodgers@gmail.com>
2013-02-16 16:43:13 -0500

    Replaced interface map with JoystickButtons

--- OI.cpp
+++ OI.cpp
@@ -5,16 +5,29 @@
  */
 
 #include "OI.h"
+#include "Commands/Shoot.h"
 
 /**
  * Initilize the Operator Interface.
  *
  * @author Nyle Rodgers
  */
-OI::OI() {
-	leftPrimaryJoystick = new Joystick(1);
-	rightPrimaryJoystick = new Joystick(2);
-	secondaryJoystick = new Joystick(3);
+OI::OI():
+	leftPrimaryJoystick( new Joystick(1) ),
+	rightPrimaryJoystick( new Joystick(2) ),
+	secondaryJoystick( new Joystick(3) ),
+	
+	oneTenthSpeedMultiplierButton( new JoystickButton( leftPrimaryJoystick, 2) ),
+	halfSpeedMultiplierButton( new JoystickButton( leftPrimaryJoystick, 1) ),
+	fullSpeedButton( new JoystickButton( rightPrimaryJoystick, 1) ),
+	halfSpeedTurningMultiplierButton( new JoystickButton( rightPrimaryJoystick, 3) ),
+	quarterSpeedTurningMultiplierButton( new JoystickButton( rightPrimaryJoystick, 5) ),
+	
+	fireButton( new JoystickButton(secondaryJoystick, 1) ),
+	setpointOneButton( new JoystickButton(secondaryJoystick, 3) ),
+	setpointTwoButton( new JoystickButton(secondaryJoystick, 4) ),
+	setpointThreeButton( new JoystickButton(secondaryJoystick, 5) ){
+	fireButton->WhenPressed(new Shoot());
 }
 
 /**
@@ -42,4 +55,41 @@ Joystick* OI::getRightPrimaryJoystick() {
  */
 Joystick* OI::getSecondaryJoystick() {
 	return secondaryJoystick;
+}
+
+bool OI::getOneTenthSpeedMultiplierButtonValue() {
+	return oneTenthSpeedMultiplierButton->Get();
+}
+
+bool OI::getHalfSpeedMultiplierButtonValue() {
+	return halfSpeedMultiplierButton->Get();
+}
+
+bool OI::getFullSpeedButtonValue() {
+	return fullSpeedButton->Get();
+}
+
+bool OI::getHalfSpeedTurningMultiplierButtonValue() {
+	return halfSpeedTurningMultiplierButton->Get();
+}
+
+
+bool OI::getQuarterSpeedTurningMultiplierButtonValue() {
+	return quarterSpeedTurningMultiplierButton->Get();
+}
+
+bool OI::getFireButtonValue() {
+	return fireButton->Get();
+}
+
+bool OI::getSetpointOneButtonValue() {
+	return setpointOneButton->Get();
+}
+
+bool OI::getSetpointTwoButtonValue() {
+	return setpointTwoButton->Get();
+}
+
+bool OI::getSetpointThreeButtonValue() {
+	return setpointThreeButton->Get();
 }
