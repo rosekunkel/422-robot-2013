47582e5c057f77abd72cd7704ae31ee5a6d0ff79
Nyle <nyle.rodgers@gmail.com>
2013-02-17 14:08:22 -0500

    Split Pneumatics into CompressorSystem and Pusher, added climber

---
+++ Subsystems/Pusher.h
@@ -0,0 +1,30 @@
+/**
+ * @file Pusher.h
+ * @brief Declaration of the Pusher subsystem
+ * @author Nyle Rodgers
+ */
+#ifndef PUSHER_H
+#define PUSHER_H
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+/**
+ * @brief The Pusher subsystem
+ * 
+ * Controls the disk pusher of the robot.
+ */
+class Pusher: public Subsystem {
+public:
+	/// Constructor for the pusher subsystem
+	Pusher();
+
+	/// Extend the disk pusher solenoid.
+	void extend();
+
+	/// Retract the disk pusher solenoid.
+	void retract();
+private:
+	DoubleSolenoid *diskPusher;
+};
+
+#endif //PUSHER_H
