b991da74f85a62fb48c6172bbaa55b53327d7b27
Nyle <nyle.rodgers@gmail.com>
2013-02-17 14:09:32 -0500

    Fixed/updated OI

--- OI.cpp
+++ OI.cpp
@@ -6,6 +6,7 @@
 
 #include "OI.h"
 #include "Commands/Shoot.h"
+#include "Commands/ToggleClimber.h"
 
 /**
  * Initilize the Operator Interface.
@@ -13,20 +14,33 @@
  * @author Nyle Rodgers
  */
 OI::OI():
+	// Joysticks
 	leftPrimaryJoystick( new Joystick(1) ),
 	rightPrimaryJoystick( new Joystick(2) ),
 	secondaryJoystick( new Joystick(3) ),
 	
-	oneTenthSpeedMultiplierButton( new JoystickButton( leftPrimaryJoystick, 2) ),
-	halfSpeedMultiplierButton( new JoystickButton( leftPrimaryJoystick, 1) ),
-	fullSpeedButton( new JoystickButton( rightPrimaryJoystick, 1) ),
-	halfSpeedTurningMultiplierButton( new JoystickButton( rightPrimaryJoystick, 3) ),
-	quarterSpeedTurningMultiplierButton( new JoystickButton( rightPrimaryJoystick, 5) ),
+	// Primary
+	oneTenthSpeedMultiplierButton( new JoystickButton( leftPrimaryJoystick, 2 ) ),
+	halfSpeedMultiplierButton( new JoystickButton( leftPrimaryJoystick, 1 ) ),
+	fullSpeedButton( new JoystickButton( rightPrimaryJoystick, 1 ) ),
 	
-	fireButton( new JoystickButton(secondaryJoystick, 1) ),
-	setpointOneButton( new JoystickButton(secondaryJoystick, 3) ),
-	setpointTwoButton( new JoystickButton(secondaryJoystick, 4) ),
-	setpointThreeButton( new JoystickButton(secondaryJoystick, 5) ){
+	halfSpeedTurningMultiplierButton( new JoystickButton( rightPrimaryJoystick, 5 ) ),
+	quarterSpeedTurningMultiplierButton( new JoystickButton( rightPrimaryJoystick, 4 ) ),
+	
+	driveReverseButton( new JoystickButton( leftPrimaryJoystick, 3 ) ),
+	
+	toggleClimberButton( new JoystickButton( rightPrimaryJoystick, 7 ) ),
+	
+	// Secondary
+	
+	fireButton( new JoystickButton( secondaryJoystick, 1 ) ),
+	setpointZeroButton( new JoystickButton ( secondaryJoystick, 2 )),
+	setpointOneButton( new JoystickButton( secondaryJoystick, 4 ) ),
+	setpointTwoButton( new JoystickButton( secondaryJoystick, 3 ) ),
+	setpointThreeButton( new JoystickButton( secondaryJoystick, 5 ) ) {
+	
+	toggleClimberButton->WhenPressed( new ToggleClimber() );
+	
 	fireButton->WhenPressed(new Shoot());
 }
 
@@ -57,39 +71,105 @@ Joystick* OI::getSecondaryJoystick() {
 	return secondaryJoystick;
 }
 
+/**
+ * Get the one tenth speed multiplier button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getOneTenthSpeedMultiplierButtonValue() {
 	return oneTenthSpeedMultiplierButton->Get();
 }
 
+/**
+ * Get the half speed multiplier button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getHalfSpeedMultiplierButtonValue() {
 	return halfSpeedMultiplierButton->Get();
 }
 
+/**
+ * Get the full speed multiplier button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getFullSpeedButtonValue() {
 	return fullSpeedButton->Get();
 }
 
+/**
+ * Get the half speed turning multiplier button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getHalfSpeedTurningMultiplierButtonValue() {
 	return halfSpeedTurningMultiplierButton->Get();
 }
 
-
+/**
+ * Get the quarter speed turning multiplier button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getQuarterSpeedTurningMultiplierButtonValue() {
 	return quarterSpeedTurningMultiplierButton->Get();
 }
 
+/**
+ * Get the drive reverse button state.
+ *
+ * @author Nyle Rodgers
+ */
+bool OI::getDriveReverseButtonValue() {
+	return driveReverseButton->Get();
+}
+
+/**
+ * Get the toggle climber button state.
+ *
+ * @author Nyle Rodgers
+ */
+bool OI::getToggleClimberButtonValue() {
+	return toggleClimberButton->Get();
+}
+
+/**
+ * Get the half speed turning multiplier button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getFireButtonValue() {
 	return fireButton->Get();
 }
 
+bool OI::getSetpointZeroButtonValue() {
+	return setpointZeroButton->Get();
+}
+
+/**
+ * Get the setpoint one button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getSetpointOneButtonValue() {
 	return setpointOneButton->Get();
 }
 
+/**
+ * Get the setpoint two button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getSetpointTwoButtonValue() {
 	return setpointTwoButton->Get();
 }
 
+/**
+ * Get the setpoint three button state.
+ *
+ * @author Nyle Rodgers
+ */
 bool OI::getSetpointThreeButtonValue() {
 	return setpointThreeButton->Get();
 }
