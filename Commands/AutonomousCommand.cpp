409f94a739228294a3e5e4feda0795c22067ea8c
William Kunkel <will@wkunkel.com>
2013-10-19 09:49:28 -0400

    Set lights to alliance colors in autonomous

--- Commands/AutonomousCommand.cpp
+++ Commands/AutonomousCommand.cpp
@@ -3,8 +3,18 @@
 #include "VariableDelay.h"
 #include "SetShooter.h"
 #include "Shoot.h"
+#include "ControlLights.h"
 
 AutonomousCommand::AutonomousCommand() {
+	ControlLights::Color allianceColor = ControlLights::GREEN;
+	DriverStation::Alliance currentAlliance = DriverStation::GetInstance()->GetAlliance();
+	if( currentAlliance == DriverStation::kRed ) {
+		allianceColor = ControlLights::RED;
+	}
+	else if( currentAlliance == DriverStation::kBlue ) {
+			allianceColor = ControlLights::BLUE;
+	}
+	AddParallel( new ControlLights(allianceColor) );
 	AddSequential( new SetShooter( 35.0 ) );
 	AddSequential( new VariableDelay() );
 	for( int i = 0; i < 3; ++i ) {
