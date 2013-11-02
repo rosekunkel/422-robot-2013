596d9174b4d9b68174973f222ae2ed1b6c2dc44c
William Kunkel <will@wkunkel.com>
2013-11-02 07:55:26 -0400

    Removed alliance color initialization

--- Commands/AutonomousCommand.cpp
+++ Commands/AutonomousCommand.cpp
@@ -6,15 +6,6 @@
 #include "ControlLights.h"
 
 AutonomousCommand::AutonomousCommand() {
-	ControlLights::Color allianceColor = ControlLights::GREEN;
-	DriverStation::Alliance currentAlliance = DriverStation::GetInstance()->GetAlliance();
-	if( currentAlliance == DriverStation::kRed ) {
-		allianceColor = ControlLights::RED;
-	}
-	else if( currentAlliance == DriverStation::kBlue ) {
-			allianceColor = ControlLights::BLUE;
-	}
-	AddParallel( new ControlLights(allianceColor) );
 	AddSequential( new SetShooter( 35.0 ) );
 	AddSequential( new VariableDelay() );
 	for( int i = 0; i < 3; ++i ) {
